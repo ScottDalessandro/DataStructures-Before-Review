ebf53ba76b114fda4879d594bea0676bdd99fc8f
Scott D'Alessandro <scott@fullstackacademy.com>
2023-12-22 10:00:44 -0500

    add file structure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const LinkedList = require('./linked-list-0.0.1-beta.js')
+const LinkedList = require('./linked-list/singly/linked-list-0.0.1-beta.js')
 
 const ll = LinkedList.fromValues(10,20,30,40)
 ll.print()
