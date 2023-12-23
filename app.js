b23e975b08c5733099cd040da9fe11041d1d8ec3
Scott D'Alessandro <scott@fullstackacademy.com>
2023-12-23 07:21:32 -0500

    add app.js to .gitignore

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const LinkedList = require('./linked-list/singly/linked-list-0.0.1-beta.js')
+const SinglyLinkedList = require('./linked-list/singly-ll-with-tail/singly-ll-with-tail.js')
 
-const ll = LinkedList.fromValues(10,20,30,40)
-ll.print()
-console.log(ll.getByIndex(0).value)
+const sll = new SinglyLinkedList()
+sll.push(5).push(4).push(3).push(2).push(1).push(0)
+const lastValueInLL = sll.pop()
+
+console.log('lastValue', lastValueInLL)
+console.log(sll)
+console.log(sll.tail.value)
