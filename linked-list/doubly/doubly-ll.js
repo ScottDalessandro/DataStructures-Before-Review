9301ac95c1ffddee6d10fa472514e94ee2bcece9
Scott D'Alessandro <scott@fullstackacademy.com>
2023-12-30 21:29:28 -0500

    add get logic

--- linked-list/doubly/doubly-ll.js
+++ linked-list/doubly/doubly-ll.js
@@ -73,4 +73,27 @@ class DoublyLinkedList {
         this.length++
         return this
     }
+
+    get(index) {
+        if(index < 0 || index >= this.length) return null
+        let node, count;
+
+        if(index <= this.length/2) {
+            count = 0
+            current = this.head
+            while(count !== index) {
+                current = current.next
+                count--
+            }
+        } else {
+            count = this.length = 1
+            current = this.tail
+
+            while(count !== index) {
+                current = current.prev
+                count--
+            }
+        }
+        return current
+    }
 }
