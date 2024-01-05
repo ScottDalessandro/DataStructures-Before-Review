37029e46a3e2f3d1346b86a363da1a1720cc5cfa
Scott D'Alessandro <scott@fullstackacademy.com>
2024-01-05 10:10:39 -0500

    initial specs for HashTable (get&put)

---
+++ hash-table/hash-table.test.js
@@ -0,0 +1,8 @@
+describe('HashTable', () => {
+    describe('get', () => {
+        test.todo('retrieves a value when provided a key')
+        test.todo('returns undefined when a key is not stored')
+    })
+
+    describe('put', () => {})
+})
