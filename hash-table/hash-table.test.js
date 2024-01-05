7f4d707430120d7f7bcb2c7bd193575d0a48780f
Scott D'Alessandro <scott@fullstackacademy.com>
2024-01-05 11:53:30 -0500

    add hash table property specs

--- hash-table/hash-table.test.js
+++ hash-table/hash-table.test.js
@@ -1,4 +1,16 @@
+const { HashTable } = require('./hash-table')
 describe('HashTable', () => {
+
+    describe('hash table properties', () => {
+        test('contains a table property', () => {
+            const hashTable = new HashTable()
+            expect(hashTable.table).toBeDefined()
+        })
+        test('contains a size property', () => {
+            const hashTable = new HashTable()
+            expect(hashTable.size).toBeDefined()
+        })
+    })
     describe('get', () => {
         test.todo('retrieves a value when provided a key')
         test.todo('returns undefined when a key is not stored')
