1f7a98c186161c678c9405a3b3e1419453003527
Scott D'Alessandro <scott@fullstackacademy.com>
2024-01-07 23:04:34 -0500

    update var

--- hash-table/ht.js
+++ hash-table/ht.js
@@ -1,7 +1,7 @@
 /* Hash Table */
 
 var hash = (string, max) => {
-    var hash = 0;
+    let hash = 0;
     for (var i = 0; i < string.length; i++) {
       hash += string.charCodeAt(i);
     }
@@ -64,14 +64,3 @@ var hash = (string, max) => {
     };
   
   };
-  
-  
-  console.log(hash('quincy', 10))
-  
-  let ht = new HashTable();
-  ht.add('beau', 'person');
-  ht.add('fido', 'dog');
-  ht.add('rex', 'dinosour');
-  ht.add('tux', 'penguin')
-  console.log(ht.lookup('tux'))
-  ht.print();
